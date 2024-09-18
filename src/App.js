69fe1eff8d37cecbd6938d551423a5a468272500
saiju <josephksaiju@gmail.com>
2024-09-18 18:34:35 +0530

    app progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import React from "react";
+import Pages from "./pages/Pages";
+import Category from "./componets/Category";
+import { BrowserRouter } from "react-router-dom";
+import Search from "./componets/Search";
 
 function App() {
   return (
     <div className="App">
-<h1>Hello React</h1>
+      <BrowserRouter>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Veggie from "../componets/Veggie";
+import Popular from "../componets/Popular";
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <Popular />
+      <Veggie />
+    </div>
+  );
+}
+
+export default Home;
